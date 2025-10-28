88a3e384d0772057201ad08dfa2279fcc8519abe
Eylon Keshet <>
2025-10-28 16:58:00 +0200

    construct note dictionary

--- scripts/note_card.gd
+++ scripts/note_card.gd
@@ -1,6 +1,8 @@
 class_name NoteCard extends Panel
 
 @export var notes: Array[float]
+var notes_dictionary: Dictionary
+enum note_status_types {ACTIVE,MISSED,PLAYED,PLAYED_BAD,INACTIVE}
 var note_timings: Array[float]
 var notes_triggered: Array[bool]
 var active_note_num: int = -1
@@ -11,11 +13,39 @@ var active: bool:
 @export var beat_num: int = 1
 var game: Game
 
+func construct_notes_dictionary(note_durations_array: Array) -> void:
+	notes_dictionary.clear()
+	var index: int = 0
+	for note in note_durations_array:
+		notes_dictionary[index] = {}
+		notes_dictionary[index]["duration"] = note
+		notes_dictionary[index]["status"] = note_status_types.INACTIVE
+		index += 1
+	calculate_note_timings()
+	print(notes_dictionary)
+
+func calculate_note_timings() -> void:
+	var time_before_card_starts: float = game.one_beat_duration * (beat_num-1)
+	#var index: int = 0
+	for i in range(notes_dictionary.size()):
+		if i not in notes_dictionary:
+			return
+		var current_note_duration: float = notes_dictionary[i]["duration"] * game.one_beat_duration / game.one_beat_value
+		var previous_note_timing: float = 0
+		if i == 0:
+			notes_dictionary[i]["timing"] = time_before_card_starts
+		elif i > 0:
+			previous_note_timing = notes_dictionary[i-1]["timing"]
+			notes_dictionary[i]["timing"] = previous_note_timing + current_note_duration
+		#index += 1
+		#print("for card " + str(name) + " note " + str(i) + " timing is " + str(notes_dictionary[i]["timing"]))
+
 func _ready() -> void:
 	game = find_parent("Game")
 	if game != null:
 		game.beat_signal.connect(toggle_by_beat)
 		game.round_changed.connect(set_note_timings.bind(notes))
+	construct_notes_dictionary(notes)
 	#reset_note_triggers()
 	#for note in notes:
 		#notes_triggered.append(false)
@@ -54,6 +84,9 @@ func toggle_by_beat(round_beat: int) -> void:
 		active = true
 	else:
 		active = false
+
+func reset_note_triggers_dictionary() -> void:
+	pass
 
 func reset_note_triggers() -> void:
 	notes_triggered.clear()
